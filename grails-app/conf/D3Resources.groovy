5cfcc343f12407fb5fbd6837b9b0b89c1a75932d
Jean-Louis Jouannic <jeanlouis.jouannic+github@gmail.com>
2014-04-08 22:21:04 +0200

    Upgrade to D3 3.4.5

--- grails-app/conf/D3Resources.groovy
+++ grails-app/conf/D3Resources.groovy
@@ -5,7 +5,7 @@ boolean dev = Environment.isDevelopmentMode()
 modules = {
 
     'd3' {
-        resource id: 'd3', url: [plugin: 'd3', dir: 'js/D3', file: "d3.v3${dev ? '' : '.min'}.js"], disposition: 'head',
+        resource id: 'd3', url: [plugin: 'd3', dir: 'js/D3', file: "d3${dev ? '' : '.min'}.js"], disposition: 'head',
                 exclude: 'minify'
     }
 }
